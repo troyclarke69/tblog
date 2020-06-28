be417cf3b87a42b872ef54cc274bcca6a3624921
Troy Clarke <troy.clarke87@gmail.com>
2020-06-28 16:57:23 -0400

    Initial

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const path = require("path")
+
+var dir = path.join(__dirname, 'public');
+
+const app = express()
+app.use(express.static(dir));
+
+//'mongodb://localhost/blog'
+mongoose.connect('mongodb+srv://troy:Gruj3267@cluster0-orxh2.gcp.mongodb.net/tblog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
